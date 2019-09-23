809b8153348ee767c6365429bc8eea9c95b9a18e
88IO <8810aoba@gmail.com>
2019-09-23 17:49:23 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -u
+
+DOT_DIR=$(cd $(dirname $0); pwd)
+
+echo "Link dotfiles"
+
+echo "Enter DOT_DIR"
+
+cd ${DOT_DIR}
+
+for d in vim tmux shell
+do
+    cd ${d}
+    for f in .??*
+    do
+        [ "$f" = ".git" ] && continue
+        [ "$f" = ".gitignore" ] && continue
+
+        ln -snfv ${DOT_DIR}/${d}/${f} ${HOME}/${f}
+    done
+    cd ${DOT_DIR}
+done
+
+ln -snfv ${DOT_DIR}/nvim ${HOME}/.config/nvim
+
+echo "linked dotfiles completely"
